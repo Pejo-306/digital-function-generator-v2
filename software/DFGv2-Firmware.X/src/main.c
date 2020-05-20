af0a332125505efba934bd5c04a7285bbd7cdfdf
Petar Nikolov <penikolov23@gmail.com>
2020-05-20 19:27:28 +0300

    [Firmware] Final version for diploma

--- software/DFGv2-Firmware.X/src/main.c
+++ software/DFGv2-Firmware.X/src/main.c
@@ -5,29 +5,20 @@
  * Created on January 25, 2020, 6:47 PM
  */
 
-#include <stdlib.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
 
+#include "defs.h"
 #include "init_mcu.h"
 #include "avr_controllers/twi_controller.h"
 #include "avr_controllers/spi_controller.h"
 #include "lcd-driver/lcd-driver.h"
-#include "lcd-driver/graphics.h"
-
-#include "lcd-driver/font8x8.h"
-
+#include "touch-panel-driver/touch-panel-driver.h"
 #include "user-interface/ui_helper.h"
 #include "user-interface/ui_main_menu.h"
 #include "user-interface/ui_options_menu.h"
-#include "touch-panel-driver/touch-panel-driver.h"
-
 #include "DFG-firmware/DDS_firmware.h"
 #include "DFG-firmware/IOexpander_driver.h"
 #include "DFG-firmware/programmable_oscillator_driver.h"
-
-#include "DFG-firmware/waveforms.h"
 
 int main(void)
 {       
@@ -36,6 +27,9 @@ int main(void)
     twi_init();
     twi_set_speed(TWI_400KHZ, 0);
     twi_set_slave_address(0x00);
+    
+    // reset MCP23017
+    ioex_reset();
 
     // initialize programmable oscillator (DS1085)
     osc_conf_mux_word(U8_TWI_ADDRESS, 0x2000);
@@ -81,7 +75,6 @@ int main(void)
         spi_set_speed(SPI_2MHZ);  // 2 MHz
         if (touch_scan(&touch, &x, &y)) {
             touch_get_screen_coordinates(&driver, &x, &y);
-            draw_pixel(&driver, x, y, 0xFFFF);
         } else {
             x = 0xFFFF;
             y = 0xFFFF;
