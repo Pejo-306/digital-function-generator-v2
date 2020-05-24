77073f6d7e26ca141757a2c3817747f885e55d42
Petar Nikolov <penikolov23@gmail.com>
2020-05-24 18:25:41 +0300

    [Firmware] Fine tune hardcoded values

--- software/DFGv2-Firmware.X/src/hardcode.c
+++ software/DFGv2-Firmware.X/src/hardcode.c
@@ -10,8 +10,8 @@ uint16_t g_wave_size = SINE_SIZE;
 size_t g_freq_setting = FREQ_SETTING_1KHZ;
 fbool g_wave_changed = FBOOL0;
 const uint16_t g_values[][5] = {
-    { 1024, 0x2780, 998, -2, 2}, // 10 Hz
-    { 1024, 0x2780, 8, -2, 2}, // 1 kHz
-    { 256, 0x2200 , 2, +2, 10 }  // 100 kHz
+    { 1024, 0x3780, 998, -2, 2}, // 10 Hz
+    { 1024, 0x3780, 8, -2, 2}, // 1 kHz
+    { 128, 0x3400, 4, -3, 10 }  // 100 kHz
     // resolution, mux word, div word, offset byte, dac word
 };
